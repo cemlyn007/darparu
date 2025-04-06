d6b838e8960686b1ddd378af90e304466bd0b69c
Cemlyn <cemlynwaters@gmail.com>
2025-04-06 22:24:11 +0100

    feat: pan camera

---
+++ darparu/renderer/cameras/pan.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "darparu/renderer/algebra.h"
+#include "darparu/renderer/camera.h"
+#include <array>
+
+namespace darparu::renderer {
+class PanCamera : public Camera {
+public:
+  PanCamera() = default;
+  ~PanCamera() = default;
+
+  virtual std::array<float, 16> update() {
+    return look_at(_position, {_position[0], _position[1], 0.0}, {0.0, 1.0, 0.0});
+  };
+};
+} // namespace darparu::renderer
